ab4cfc3b59d4e8b88735d1ec8d06a3ff52bcfe09
李阳 <admin@admindeMacBook-Air.local>
2017-08-06 16:30:26 +0800

    26-5 报名15分钟内没完成自动取消

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,7 @@
 class RegistrationsController < ApplicationController
 
   before_action :find_event
+  before_action :set_pending_registration, :only => [:step1, :step1_update, :step2, :step2_update, :step3, :step3_update]
 
   def new
   end
@@ -13,6 +14,7 @@ class RegistrationsController < ApplicationController
     @registration.current_step = 1
 
     if @registration.save
+      CheckRegistrationJob.set( wait: 15.minutes ).perform_later(@registration.id)
       redirect_to step2_event_registration_path(@event, @registration)
     else
       flash.now[:alert] = @registration.errors[:base].join("、")
@@ -24,12 +26,22 @@ class RegistrationsController < ApplicationController
     @registration = @event.registrations.find_by_uuid(params[:id])
   end
 
+  def step1
+  end
+
+  def step1_update
+
+    if @registration.update(registration_params)
+      redirect_to step2_event_registration_path(@event, @registration)
+    else
+      render "step1"
+    end
+  end
+
   def step2
-    @registration = @event.registrations.find_by_uuid(params[:id])
   end
 
   def step2_update
-    @registration = @event.registrations.find_by_uuid(params[:id])
     @registration.current_step = 1
 
     if @registration.update(registration_params)
@@ -40,11 +52,9 @@ class RegistrationsController < ApplicationController
   end
 
   def step3
-    @registration = @event.registrations.find_by_uuid(params[:id])
   end
 
   def step3_update
-    @registration = @event.registrations.find_by_uuid(params[:id])
     @registration.status = "confirmed"
     @registration.current_step = 1
 
@@ -53,25 +63,12 @@ class RegistrationsController < ApplicationController
 
       NotificationMailer.confirmed_registration(@registration).deliver_later
       redirect_to event_registration_path(@event, @registration)
-      
+
     else
       render "step3"
     end
   end
 
-  def step1
-    @registration = @event.registrations.find_by_uuid(params[:id])
-  end
-
-  def step1_update
-    @registration = @event.registrations.find_by_uuid(params[:id])
-
-    if @registration.update(registration_params)
-      redirect_to step2_event_registration_path(@event, @registration)
-    else
-      render "step1"
-    end
-  end
 
 
   protected
@@ -83,4 +80,13 @@ class RegistrationsController < ApplicationController
   def find_event
     @event = Event.find_by_friendly_id(params[:event_id])
   end
+
+   def set_pending_registration
+     @registration = @event.registrations.find_by_uuid(params[:id])
+
+     if @registration.status == "cancalled"
+       flash[:alert] = "请重新报名"
+       redirect_to event_path(@event)
+     end
+   end
 end
